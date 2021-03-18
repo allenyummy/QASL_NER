6f626a69ebe42180c6f8f79745f753eaf2c16850
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-03-18 16:57:09 +0800

    [modified] stat

--- utils/data_structure/stat.py
+++ utils/data_structure/stat.py
@@ -8,37 +8,49 @@ from typing import NamedTuple
 logger = logging.getLogger(__name__)
 
 
+class Stat:
+    n_sentence: int = 0
+    n_token: int = 0
+    n_entity: int = 0
+    layer: int = 0
+
+
 class GENIA_StatStruct:
     n_sentence: int = 0
-    n_tokens: int = 0
+    n_token: int = 0
+    n_entity: int = 0
 
-    n_entities: int = 0
+    DNA: Stat = Stat()
+    RNA: Stat = Stat()
+    protein: Stat = Stat()
+    cell_line: Stat = Stat()
+    cell_type: Stat = Stat()
     n_DNA: int = 0
     n_RNA: int = 0
     n_protein: int = 0
     n_cell_line: int = 0
     n_cell_type: int = 0
 
-    n_tokens_per_sentence: float = 0.0
-    n_entities_per_sentence: float = 0.0
+    n_token_per_sentence: float = 0.0
+    n_entity_per_sentence: float = 0.0
 
     def calc_average(self):
         if self.n_sentence != 0:
-            self.n_tokens_per_sentence = round(self.n_tokens / self.n_sentence, 2)
-            self.n_entities_per_sentence = round(self.n_entities / self.n_sentence, 2)
+            self.n_token_per_sentence = round(self.n_token / self.n_sentence, 2)
+            self.n_entity_per_sentence = round(self.n_entity / self.n_sentence, 2)
 
     def __repr__(self):
         return (
-            f"Total number of sentence:      {self.n_sentence}\n"
-            f"Total number of tokens:        {self.n_tokens}\n"
-            f"Total number of entities:      {self.n_entities}\n"
-            f"Average tokens per sentence:   {self.n_tokens_per_sentence}\n"
-            f"Average entities per sentence: {self.n_entities_per_sentence}\n"
-            f"Total number of entities of DNA:       {self.n_DNA}\n"
-            f"Total number of entities of RNA:       {self.n_RNA}\n"
-            f"Total number of entities of protein:   {self.n_protein}\n"
-            f"Total number of entities of cell line: {self.n_cell_line}\n"
-            f"Total number of entities of cell type: {self.n_cell_type}\n"
+            f"Total number of sentence in data set  : {self.n_sentence}\n"
+            f"Total number of tokens in data set    : {self.n_token}\n"
+            f"Total number of entities in data set  : {self.n_entity}\n"
+            f"Average number of token per sentence  : {self.n_token_per_sentence}\n"
+            f"Average number of entity per sentence : {self.n_entity_per_sentence}\n"
+            f"Total number of entities of DNA       : {self.n_DNA}\n"
+            f"Total number of entities of RNA       : {self.n_RNA}\n"
+            f"Total number of entities of protein   : {self.n_protein}\n"
+            f"Total number of entities of cell line : {self.n_cell_line}\n"
+            f"Total number of entities of cell type : {self.n_cell_type}\n"
         )
 
 

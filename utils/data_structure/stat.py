a3d919315efa10709fd81f06e0e82eeebb7fd2b0
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-03-19 17:00:58 +0800

    [modified] stat

--- utils/data_structure/stat.py
+++ utils/data_structure/stat.py
@@ -1,41 +1,48 @@
 # encoding=utf-8
 # Author: Yu-Lun Chiang
-# Discription: Data Structure of Statitics
+# Discription: Data Structure of Statistics
 
 import logging
-from typing import NamedTuple
+from typing import List, Dict
 
 logger = logging.getLogger(__name__)
 
 
-class Stat:
+class TypeStatStruct:
+    n_entity: int = 0
+    layer: List[int] = list()
+
+    def __init__(self, type: str):
+        self._type = type
+
+    @property
+    def type(self):
+        return self._type
+
+    def __repr__(self):
+        return (
+            f"--------- {self.type} ---------\n"
+            f"n_entity               : {self.n_entity}\n"
+            f"n_entity in each layer : {self.layer}\n"
+        )
+
+
+class StatStruct:
     n_sentence: int = 0
     n_token: int = 0
     n_entity: int = 0
-    layer: int = 0
-
-
-class GENIA_StatStruct:
-    n_sentence: int = 0
-    n_token: int = 0
-    n_entity: int = 0
-
-    DNA: Stat = Stat()
-    RNA: Stat = Stat()
-    protein: Stat = Stat()
-    cell_line: Stat = Stat()
-    cell_type: Stat = Stat()
-    n_DNA: int = 0
-    n_RNA: int = 0
-    n_protein: int = 0
-    n_cell_line: int = 0
-    n_cell_type: int = 0
-
     n_token_per_sentence: float = 0.0
     n_entity_per_sentence: float = 0.0
+    each_type_stat: Dict[str, TypeStatStruct] = dict()
 
-    def calculate(self):
-        raise NotImplementedError
+    def __init__(self, type_list):
+        self._type_list = type_list
+        for type in self.type_list:
+            self.each_type_stat[type] = TypeStatStruct(type)
+
+    @property
+    def type_list(self):
+        return self._type_list
 
     def calc_average(self):
         if self.n_sentence != 0:
@@ -43,20 +50,22 @@ class GENIA_StatStruct:
             self.n_entity_per_sentence = round(self.n_entity / self.n_sentence, 2)
 
     def __repr__(self):
+        newline = "\n"
         return (
             f"Total number of sentence in data set  : {self.n_sentence}\n"
             f"Total number of tokens in data set    : {self.n_token}\n"
             f"Total number of entities in data set  : {self.n_entity}\n"
             f"Average number of token per sentence  : {self.n_token_per_sentence}\n"
             f"Average number of entity per sentence : {self.n_entity_per_sentence}\n"
-            f"Total number of entities of DNA       : {self.n_DNA}\n"
-            f"Total number of entities of RNA       : {self.n_RNA}\n"
-            f"Total number of entities of protein   : {self.n_protein}\n"
-            f"Total number of entities of cell line : {self.n_cell_line}\n"
-            f"Total number of entities of cell type : {self.n_cell_type}\n"
+            f'{newline.join(f"{typestat}" for type, typestat in self.each_type_stat.items())}'
         )
 
 
 if __name__ == "__main__":
-    a = GENIA_StatStruct()
+    TYPE_LIST = ["G#DNA", "G#RNA", "G#protein", "G#cell_line", "G#cell_type"]
+    a = StatStruct(TYPE_LIST)
+    print(a)
+    print("-------")
+    a.each_type_stat["G#DNA"].layer = a.each_type_stat["G#DNA"].layer.copy() + [0]
+    a.each_type_stat["G#DNA"].layer[0] += 1
     print(a)

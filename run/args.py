363e463464218d269f9c446b3a1484566fde1074
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-03-26 16:14:10 +0800

    [init] arguments

---
+++ run/args.py
@@ -0,0 +1,78 @@
+# encoding=utf-8
+# Author: Yu-Lun Chiang
+# Description: Definition of Custom Arguments
+
+from typing import List, Optional
+from dataclasses import dataclass, field
+
+
+@dataclass
+class DataTrainingArguments:
+    """
+    Arguments pertaining to what data we are going to input our model for training and eval.
+    """
+
+    dataset_name: str = field(
+        metadata={"help": "The name of the dataset to use (via the datasets library)."},
+    )
+    dataset_script_file: str = field(
+        metadata={"help": "The script file of loading dataset."}
+    )
+    dataset_config_name: str = field(
+        metadata={
+            "help": "The configuration name of the dataset to use (via the datasets library)."
+        },
+    )
+    data_dir: Optional[str] = field(
+        default=None,
+        metadata={"help": "The input data dir."},
+    )
+    train_file: Optional[str] = field(
+        default=None,
+        metadata={"help": "The input training data file (a csv or JSON file)."},
+    )
+    validation_file: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "An optional input evaluation data file to evaluate on (a csv or JSON file)."
+        },
+    )
+    test_file: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "An optional input test data file to predict on (a csv or JSON file)."
+        },
+    )
+    overwrite_cache: bool = field(
+        default=False,
+        metadata={"help": "Overwrite the cached training and evaluation sets"},
+    )
+    preprocessing_num_workers: Optional[int] = field(
+        default=None,
+        metadata={"help": "The number of processes to use for the preprocessing."},
+    )
+
+
+@dataclass
+class ModelArguments:
+    """
+    Arguments pertaining to which model/config/tokenizer we are going to fine-tune from.
+    """
+
+    model_name_or_path: str = field(
+        metadata={
+            "help": "Path to pretrained model or model identifier from huggingface.co/models"
+        }
+    )
+    config_name: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Pretrained config name or path if not the same as model_name"
+        },
+    )
+    tokenizer_name: Optional[str] = field(
+        default=None,
+        metadata={
+            "help": "Pretrained tokenizer name or path if not the same as model_name"
+        },
+    )

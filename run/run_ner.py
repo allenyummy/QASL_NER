1b01bbc3c7df4850b66aef26a190fe1efe55f959
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-03-30 15:53:07 +0800

    [modified] create features

--- run/run_ner.py
+++ run/run_ner.py
@@ -44,7 +44,6 @@ def main():
         bool(training_args.local_rank != -1),
         training_args.fp16,
     )
-    globals.
 
     logger.info("============ Set Seed ============")
     set_seed(training_args.seed)
@@ -67,16 +66,27 @@ def main():
         cache_dir=data_args.data_dir,
     )
     logger.debug(dataset)
+    logger.debug(dataset["train"][0])
+
+    logger.info("============ Set Global Variables ============")
+    globals.max_seq_length = data_args.max_seq_length
+    globals.doc_stride = data_args.doc_stride
+    globals.padding_strategy = data_args.padding_strategy
+    globals.label_strategy = data_args.label_strategy
+    globals.pad_on_right = globals.tokenizer.padding_side == "right"
+    if data_args.label_strategy == "iob2":
+        globals.label_to_id = {"O": 0, "B": 1, "I": 2}
+    elif data_args.label_strategy == "iobes":
+        globals.label_to_id = {"O": 0, "B": 1, "I": 2, "E": 3, "S": 4}
 
     logger.info("============ Create Features ============")
-    train_dataset = dataset["train"]
+    train_dataset = dataset["train"].select(range(1))
     train_dataset = train_dataset.map(
         tokenize_and_align_labels,
-        batched=True,
-        num_proc=None,
-        load_from_cache_file=False,
+        batched=False,
+        load_from_cache_file=not data_args.overwrite_cache,
     )
-    print(train_dataset[0])
+    logger.debug(train_dataset[0])
 
     logger.info("============ Set DataCollator ============")
 

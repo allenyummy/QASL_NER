41118780aa91f08fe87dbad0ef493e008878fb7d
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-03-30 15:51:50 +0800

    [add] label_to_id

--- run/globals.py
+++ run/globals.py
@@ -1,6 +1,6 @@
 # encoding=utf-8
 # Author: Yu-Lun Chiang
-# Description: Global Variables
+# Description: Global Variables to be used in feature_generation.py
 
 
 global tokenizer
@@ -8,3 +8,5 @@ global max_seq_length
 global doc_stride
 global padding_strategy
 global label_strategy
+global pad_on_right
+global label_to_id

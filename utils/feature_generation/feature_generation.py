1787516cdc5528bd37455fffea26d4f77bec4ec5
Yu-Lun Chiang <chiangyulun0914@gmail.com>
2021-03-31 14:18:22 +0800

    [modified] tokenize and align labels

--- utils/feature_generation/feature_generation.py
+++ utils/feature_generation/feature_generation.py
@@ -15,14 +15,18 @@ from utils.data_structure.tag_scheme import IOB2, IOBES
 logger = logging.getLogger(__name__)
 
 
-def tokenize_and_align_labels(examples):
+def tokenize_and_align_labels(batched_examples):
 
-    question_tokens = globals.tokenizer.tokenize(examples["question"])
-    passage_tokens = examples["passage_tokens"]
+    # Prepare question_tokens and passage_tokens
+    batched_question_tokens = list()
+    for question in batched_examples["question"]:
+        batched_question_tokens.append(globals.tokenizer.tokenize(question))
+    batched_passage_tokens = batched_examples["passage_tokens"]
 
-    tokenized_inputs = globals.tokenizer(
-        question_tokens if globals.pad_on_right else passage_tokens,
-        passage_tokens if globals.pad_on_right else question_tokens,
+    # Prepare Features (input_ids, token_type_ids, attention_masks)
+    batched_tokenized_inputs = globals.tokenizer(
+        batched_question_tokens if globals.pad_on_right else batched_passage_tokens,
+        batched_passage_tokens if globals.pad_on_right else batched_question_tokens,
         is_split_into_words=True,
         truncation="only_second" if globals.pad_on_right else "only_first",
         padding=globals.padding_strategy,
@@ -30,77 +34,54 @@ def tokenize_and_align_labels(examples):
         stride=globals.doc_stride,
         return_overflowing_tokens=True,
     )
-    # for i in range(len(tokenized_inputs["input_ids"])):
-    #     print(globals.tokenizer.convert_ids_to_tokens(tokenized_inputs["input_ids"][i]))
-    #     print(tokenized_inputs.word_ids(batch_index=i))
-    #     print()
+    # Since one example might give us several features if it has a long context, we need a map from a feature to
+    # its corresponding example. This key gives us just that.
+    sample_mapping = batched_tokenized_inputs.pop("overflow_to_sample_mapping")
 
-    # -100
-    pad_token_label_id = CE().ignore_index
-
+    # Align label_ids to each example and stack back to the batch.
     labels = list()
-    for i in range(len(tokenized_inputs)):
-        word_ids = tokenized_inputs.word_ids(batch_index=i)
-        prev_word_idx = None
+    pad_token_label_id = CE().ignore_index  # -100
+    batched_answers = batched_examples["answers"]
+    for idx, example_id in enumerate(sample_mapping):
         label_ids = list()
 
-        for start, end in zip(
-            examples["answers"]["start_pos"], examples["answers"]["end_pos"]
+        # [None, 0, 1, 2, 3, None, 0, 0, 1, 2, 3, 3, 4, 5, 6, 7, ... None]
+        word_ids = batched_tokenized_inputs.word_ids(batch_index=idx)
+
+        # The position of first [SEP] token
+        first_sep_token_in_word_ids = word_ids.index(None, 1)
+
+        # The first passage tokens is behind [SEP] tokens
+        first_passage_token_in_word_ids = first_sep_token_in_word_ids + 1
+
+        # [CLS] Question [SEP] Passage [SEP] [PAD] ..
+        #  -100, -100... -100
+        label_ids.extend([pad_token_label_id] * (first_sep_token_in_word_ids + 1))
+
+        # [CLS] Question [SEP] Passage [SEP] [PAD] ...
+        #                      ........ -100 -100 ...
+        answers = batched_answers[example_id]
+        for type, text, start, end in zip(
+            answers["type"], answers["text"], answers["start_pos"], answers["end_pos"]
         ):
-
-            if globals.label_strategy.lower() == LabelStrategy.IOB2._name_.lower():
-
-                label_ids = list()
-                for word_idx in word_ids:
-                    # --- [CLS], [SEP], [PAD], [UNK] ---
-                    # Special tokens have a word id that is None.
-                    # We set the label to pad_token_label_id (-100) so they are automatically ignored in the loss function.
-                    if word_ids is None:
-                        label_ids.append(pad_token_label_id)
-
-                    # We set the label for the first token of each word.
-                    elif word_idx != prev_word_idx:
-                        if word_idx == start:
+            prev_word_id = None
+            for word_id in word_ids[first_passage_token_in_word_ids:]:
+                if word_id is None:
+                    label_ids.append(pad_token_label_id)
+                elif word_id != prev_word_id:
+                    if (
+                        globals.label_strategy.lower()
+                        == LabelStrategy.IOB2._name_.lower()
+                    ):
+                        if word_id == start:
                             label_ids.append(globals.label_to_id[IOB2.BEGINNING.value])
-                        elif start < word_idx <= end:
+                        elif start < word_id <= end:
                             label_ids.append(globals.label_to_id[IOB2.INSIDE.value])
                         else:
                             label_ids.append(globals.label_to_id[IOB2.OUTSIDE.value])
-
-                    # For the other tokens in a word, we set the label to -100.
-                    else:
-                        label_ids.append(pad_token_label_id)
-
-            elif globals.label_strategy.lower() == LabelStrategy.IOBES._name_.lower():
-                raise NotImplementedError
-            else:
-                raise ValueError("{globals.label_strategy} does not supported !!")
+                else:
+                    label_ids.append(pad_token_label_id)
+                prev_word_id = word_id
         labels.append(label_ids)
-    tokenized_inputs["labels"] = labels
-    return tokenized_inputs
-
-    # labels = []
-    # for i, label in enumerate(examples[label_column_name]):
-    #     word_ids = tokenized_inputs.word_ids(batch_index=i)
-    #     previous_word_idx = None
-    #     label_ids = []
-    #     for word_idx in word_ids:
-    #         # Special tokens have a word id that is None. We set the label to -100 so they are automatically
-    #         # ignored in the loss function.
-    #         if word_idx is None:
-    #             label_ids.append(-100)
-    #         # We set the label for the first token of each word.
-    #         elif word_idx != previous_word_idx:
-    #             label_ids.append(label_to_id[label[word_idx]])
-    #         # For the other tokens in a word, we set the label to either the current label or -100, depending on
-    #         # the label_all_tokens flag.
-    #         else:
-    #             label_ids.append(-100)
-    #             # label_ids.append(
-    #             #     label_to_id[label[word_idx]] if data_args.label_all_tokens else -100
-    #             # )
-    #         previous_word_idx = word_idx
-
-    #     labels.append(label_ids)
-    # tokenized_inputs["labels"] = labels
-    # return tokenized_inputs
+    batched_tokenized_inputs["labels"] = labels
+    return batched_tokenized_inputs
